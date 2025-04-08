d8bb1db479bb451c64bc821aeb3b7d1e11805c35
Sardorbek0907 <Sardorbek10maktab@gmail.com>
2025-04-08 17:41:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 document.getElementById('submitButton').addEventListener('click', function() {
   let link = document.getElementById('linkInput').value;
 
-  // Linkni tekshirib, serverga yuborish
+  // Agar foydalanuvchi linkni kiritgan bo'lsa
   if (link) {
-      fetch('https://yourapi.com/api/v2', {
+      const selectedService = {
+          service: 1, // Masalan, Followers servisi
+          name: "Followers",
+          type: "Default",
+          category: "First Category",
+          rate: "0.90",
+          min: "50",
+          max: "10000",
+          refill: true,
+          cancel: true
+      };
+
+      const requestData = {
+          service: selectedService.service, // Tanlangan xizmatni yuborish
+          link: link, // Linkni yuborish
+          amount: 100 // Xizmatni miqdori (masalan, 100)
+      };
+
+      // APIga so'rov yuborish
+      fetch('https://snmspeedpanel.com/api/v2', {
           method: 'POST',
           headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer e6055cfae4f628d2772aad0035c9f9f0', // API keyni qo‘shish
           },
-          body: JSON.stringify({ link: link })
+          body: JSON.stringify(requestData) // Data ni JSON shaklida yuboramiz
       })
-      .then(response => response.json())
+      .then(response => response.json()) // APIdan kelgan javobni JSON formatida olish
       .then(data => {
-          document.getElementById('response').innerText = 'Natija: ' + data.message;
+          // APIdan kelgan javobni ko‘rsatish
+          document.getElementById('response').innerText = 'Natija: ' + JSON.stringify(data);
       })
       .catch(error => {
+          // Xatolik yuz berganda xabar ko‘rsatish
           document.getElementById('response').innerText = 'Xatolik yuz berdi: ' + error;
-      });
+      });x``
   } else {
       alert('Iltimos, linkni kiriting');
   }
